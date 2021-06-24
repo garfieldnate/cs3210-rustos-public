42c7c441b8ed8582dc8cba785dcf93b2c0a9df34
Nathan Glenn <garfieldnate@gmail.com>
2021-06-24 22:42:43 +0200

    update tools
    * newer versions of clang and lld
    * current nightly version
    * cargo-xbuild documentation says to use cargo's built-in `build-std`, so we'll try that
    * use latest version of cargo-binutils
    * user doesn't always need to add .cargo/bin to PATH, since it might be done by the script

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,7 @@ set -e
 TOP=$(git rev-parse --show-toplevel)
 BIN=$TOP/bin
 DEP=$TOP/.dep
-VER=nightly-2019-07-01
+VER=nightly-2021-06-23
 PROJ_PKG=(build-essential
      python3
      socat
@@ -13,8 +13,8 @@ PROJ_PKG=(build-essential
      curl
      tar
      screen
-     clang-8
-     lld-8
+     clang-13
+     lld-11
      linux-image-extra-virtual)
 QEMU_DEP=(libglib2.0-dev libpixman-1-dev zlib1g-dev)
 
@@ -39,26 +39,8 @@ fi
 rustup default $VER
 rustup component add rust-src llvm-tools-preview clippy
 
-# install cargo xbuild
-mkdir -p $DEP
-pushd $DEP
-if ! [ -e cargo-xbuild ]; then
-  git clone https://github.com/rust-osdev/cargo-xbuild
-  pushd cargo-xbuild
-  git checkout v0.5.20
-  # https://github.com/rust-osdev/cargo-xbuild/pull/75
-  git cherry-pick b24c849028eb7da2375288b1b8ab6a7538162bd7
-  popd
-fi
-cargo install -f --path cargo-xbuild --locked
-popd
-
-# install cargo binutils
-pushd $DEP
 if ! [ -e cargo-objcopy ]; then
-  git clone https://github.com/man9ourah/cargo-binutils.git
-  cargo install -f --path cargo-binutils --locked
+  cargo install cargo-binutils --locked
 fi
-popd
 
-echo "[!] Please add '$HOME/.cargo/bin' in your PATH"
+echo "[!] Please ensure '$HOME/.cargo/bin' is on your PATH"
